48de20300650ee3dcf638d328221552a940d7883
markocdroid <markoc.dev@gmail.com>
2025-12-16 14:02:51 -0500

    Practice task

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -3,6 +3,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const removeTaskBtn = document.getElementById("removeTaskBtn")
 
 let tasks = [];
 
@@ -36,6 +37,12 @@ function clearCompletedTasks() {
     displayTasks();
 }
 
+function clearAllTasks() {
+    tasks = [];
+    displayTasks();
+}
+
 addTaskBtn.addEventListener("click", addTask);
+removeTaskBtn.addEventListener("click", clearAllTasks)
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
 
